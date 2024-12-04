4c8eb43228d257800ff32b19bb6450e31764de88
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-04 16:18:32 +0530

    User model, cotrollers & routes done in backend

--- backend/index.js
+++ backend/index.js
@@ -1,16 +1,17 @@
 import dotenv from "dotenv";
 import express from "express";
 import { connectDB } from "./db/connectDB.js";
+import authRoutes from "./routes/user.route.js";
 
 const app = express();
 
 dotenv.config();
 
-app.get("/", (req, res) => {
-  res.send("Hello World");
-});
+const PORT = process.env.PORT
 
-app.listen(3000, () => {
+app.use("/api/auth", authRoutes);
+
+app.listen(PORT, () => {
   connectDB();
-  console.log("Server listening on PORT 3000");
+  console.log("Server listening on PORT: ", PORT);
 });
