efccec3124808ef4d06cc99711a8cad80c609f3b
Dilan Pramodhya <dilanpramodhya44@gmail.com>
2024-12-12 14:32:47 +0530

    Login page and Loding indicator done in frontend

--- frontend/src/pages/LoginPage.jsx
+++ frontend/src/pages/LoginPage.jsx
@@ -1,14 +1,15 @@
 import { motion } from "framer-motion";
 import Input from "../components/Input";
-import { Lock, Mail } from "lucide-react";
+import { Loader, Lock, Mail } from "lucide-react";
 import { useState } from "react";
 import { Link } from "react-router-dom";
 
 const LoginPage = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const isLoading = false;
 
-  const handleSignUp = (e) => {
+  const handleLogin = (e) => {
     e.preventDefault();
   };
   return (
@@ -20,9 +21,9 @@ const LoginPage = () => {
     >
       <div className="p-8">
         <h2 className="text-5xl font-bold mb-8 text-center bg-gradient-to-r from-blue-400 to-cyan-400 text-transparent bg-clip-text">
-          Log In
+          Welcome Back
         </h2>
-        <form onSubmit={handleSignUp}>
+        <form onSubmit={handleLogin}>
           <Input
             icon={Mail}
             type="email"
@@ -38,18 +39,26 @@ const LoginPage = () => {
             onChange={(e) => setPassword(e.target.value)}
           />
 
-          {/* Password Strength Meter */}
+          <div className="flex items-center mb-2">
+            <Link
+              to={"/forgotPassword"}
+              className="text-sm text-blue-300 hover:underline"
+            >
+              Forgot Password
+            </Link>
+          </div>
 
           <motion.button
             className="mt-5 w-full py-3 bg-gradient-to-r from-blue-500 to-cyan-500 text-white 
-      font-bold rounded-lg shadow-lg hover:from-blue-600 hover:to-cyan-600 focus:outline-none focus:ring-2 focus:ring-blue-500 
-      focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
+            font-bold rounded-lg shadow-lg hover:from-blue-600 hover:to-cyan-600 focus:outline-none focus:ring-2 focus:ring-blue-500 
+            focus:ring-offset-2 focus:ring-offset-gray-900 transition duration-200"
             whileHover={{ scale: 1.02 }}
             whileTap={{ scale: 0.98 }}
             type="submit"
+            disabled={isLoading}
           >
-            Log In
-          </motion.button>
+            {isLoading ? <Loader className="size-6 animate-spin mx-auto" /> : "Log In"}
+            {/* Log In */}</motion.button>
         </form>
       </div>
       <div className="px-8 py-4 bg-gray-900 bg-opacity-50 flex items-center justify-center">
